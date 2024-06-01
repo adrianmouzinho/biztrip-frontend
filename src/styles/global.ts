19394785abf668f20b383e661698255b2036451d
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-06-01 10:53:06 -0300

    style: add css reset

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,5 +12,29 @@ export const globalStyles = globalCss({
 		color: '$gray900',
 		fontFamily: '$default',
 		'-webkit-font-smoothing': 'antialiased',
+		scrollBehavior: 'smooth',
+	},
+
+	'h1, h2, h3, h4, h5, h6': {
+		fontSize: 'inherit',
+		fontWeight: 'inherit',
+	},
+
+	'input, button, textarea, select': {
+		font: 'inherit',
+	},
+
+	'ol, ul': {
+		listStyle: 'none',
+	},
+
+	'img, svg': {
+		display: 'block',
+		verticalAlign: 'middle',
+	},
+
+	img: {
+		maxWidth: '100%',
+		height: 'auto',
 	},
 })
