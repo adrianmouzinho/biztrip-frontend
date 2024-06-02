49f94ffd94aacff1ecb7c563ff2acbb5cc3e009f
Adrian Mouzinho <adrianmouzinhopro@gmail.com>
2024-06-02 12:24:10 -0300

    style: customize toast

--- src/app.tsx
+++ src/app.tsx
@@ -1,34 +1,35 @@
+import {
+	CheckCircledIcon,
+	ExclamationTriangleIcon,
+	InfoCircledIcon,
+} from '@radix-ui/react-icons'
 import { RouterProvider } from 'react-router-dom'
 import { Toaster } from 'sonner'
 
 import { router } from './routes'
 
-import errorIcon from '@/assets/icons/error.svg'
-import infoIcon from '@/assets/icons/info.svg'
-import successIcon from '@/assets/icons/success.svg'
-import warningIcon from '@/assets/icons/warning.svg'
+const toastOptions = {
+	classNames: {
+		toast: 'toast',
+		error: 'toast-error',
+		success: 'toast-success',
+		warning: 'toast-warning',
+		info: 'toast-info',
+	},
+}
+
+const icons = {
+	success: <CheckCircledIcon />,
+	info: <InfoCircledIcon />,
+	warning: <ExclamationTriangleIcon />,
+	error: <ExclamationTriangleIcon />,
+}
 
 export function App() {
 	return (
 		<>
 			<RouterProvider router={router} />
-			<Toaster
-				toastOptions={{
-					classNames: {
-						toast: 'toast',
-						error: 'toast-error',
-						success: 'toast-success',
-						warning: 'toast-warning',
-						info: 'toast-info',
-					},
-				}}
-				icons={{
-					success: <img src={successIcon} alt="" />,
-					info: <img src={infoIcon} alt="" />,
-					warning: <img src={warningIcon} alt="" />,
-					error: <img src={errorIcon} alt="" />,
-				}}
-			/>
+			<Toaster toastOptions={toastOptions} icons={icons} />
 		</>
 	)
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -44,25 +44,44 @@ export const globalStyles = globalCss({
 		padding: '$6',
 		boxShadow: 'none',
 		color: '$gray600',
+		fontSize: '$xs',
+		lineHeight: '$base',
+		fontWeight: '$medium',
 	},
 
 	'.toast-error': {
 		background: '$red100',
 		border: '1px solid $red500',
+
+		svg: {
+			color: '$red500',
+		},
 	},
 
 	'.toast-success': {
 		background: '$green100',
 		border: '1px solid $green500',
+
+		svg: {
+			color: '$green500',
+		},
 	},
 
 	'.toast-warning': {
 		background: '$orange100',
 		border: '1px solid $orange500',
+
+		svg: {
+			color: '$orange500',
+		},
 	},
 
 	'.toast-info': {
 		background: '$blue100',
 		border: '1px solid $blue500',
+
+		svg: {
+			color: '$blue500',
+		},
 	},
 })
